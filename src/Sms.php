1a547fbf6d9cf6b359844a642163d843773f938f
Can Erdoğan <can@canerdogan.net>
2024-07-11 19:05:47 +0300

    PHP 8.1 compatible

--- src/Sms.php
+++ src/Sms.php
@@ -7,29 +7,39 @@ use Mukellef\Sms\Exceptions\DriverNotFoundException;
 
 class Sms
 {
+
     private $driver;
 
-    public function __construct($driver = false, $config = [])
-    {
-        if ($config === [] && function_exists('config')) {
-            $smsConfig = config('sms');
-        }
 
-        if ($driver == false) {
+    public function __construct($driver = null, array $config = [])
+    {
+
+        $this->configureDriver($driver, $config);
+    }
+
+
+    public function driver($driver = null, array $config = [])
+    {
+
+        $this->configureDriver($driver, $config);
+    }
+
+
+    private function configureDriver(?string $driver, array $config)
+    {
+
+        $smsConfig = $config === [] && function_exists('config') ? config('sms') : [];
+
+        if ($driver === null && !empty($smsConfig)) {
             $driver = $smsConfig['default_driver'];
         }
 
         if ($config === [] && function_exists('config')) {
-            $config = config('sms.'.$driver);
+            $config = config('sms.' . $driver);
         }
 
-        $this->driver($driver, $config);
-    }
-
-    public function driver($driver, $config)
-    {
         try {
-            $driverClass = "\\Mukellef\\Sms\\Drivers\\{$driver}";
+            $driverClass  = "\\Mukellef\\Sms\\Drivers\\{$driver}";
             $this->driver = new $driverClass($driver, $config);
         } catch (DriverConfigurationException $e) {
             throw new DriverConfigurationException($e);
@@ -38,13 +48,15 @@ class Sms
         }
     }
 
+
     /**
      * @param $message string
      * @param $numbers array
      * @param $header string  SMS HEADER
      */
-    public function send($message, $numbers, $header, $valid_for = '24:00')
+    public function send(string $message, array $numbers, string $header, ?string $valid_for = '24:00'): void
     {
+
         $this->driver->send($message, $numbers, $header, $valid_for);
     }
 }
